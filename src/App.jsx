49d0c9f31b9bbeda935562ba99f394a839e888b8
shumbushofred <shumbushofred@gmail.com>
2025-03-07 18:13:33 -0600

    personal website
    running and developing a personal website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Nav/Navbar'
+import Hero from './Components/Nav/Hero/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App

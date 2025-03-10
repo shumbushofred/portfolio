2b8cb590df7f4fe87efa130d3d57054c3444c3e3
shumbushofred <shumbushofred@gmail.com>
2025-03-09 22:46:56 -0500

    changes to the home page
    made the home page design better

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/linkedIn Fred-modified.png'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/linkedIn Fred-modified.png';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-        
-    <img src={profile_img} alt="" />
-    <h1><span>I'm Shumbusho Fred,</span>Full Stack Developer in the usa</h1>
-    <p>Building websites and web applications that are fast, responsive and accessible. dumiwe koko</p>
-    <div className="hero-action">
+      <div className='hero-content'>
+        <div className='hero-text'>
+          <h1>
+            <span className='hello-intro'>Hello, I am</span>
+            <span className='name-text'>Fred Shumbusho</span>
+          </h1>
+          <p className='description-text'>a passionate backend developer with a solid computer science foundation, dedicated to crafting secure and scalable APIs and cloud solutions</p>
+        </div>
+        <img src={profile_img} alt="" />
+      </div>
+      <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume">My Resume</div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
